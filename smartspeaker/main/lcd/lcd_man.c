fad182165a85159c68ddb303917dc903116c8439
Thom de Wit <tj.dewit2@student.avans.nl>
2024-03-04 10:47:47 +0100

    Fixed documentation LCD

--- smartspeaker/main/lcd/lcd_man.c
+++ smartspeaker/main/lcd/lcd_man.c
@@ -1,7 +1,6 @@
 #include <inttypes.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <sys/time.h>
 #include <hd44780.h>
 #include <pcf8574.h>
 #include <string.h>
@@ -11,13 +10,6 @@ static hd44780_t lcd;
 
 #define HD44780_ROWS 4
 #define HD44780_COLS 20
-
-static uint32_t get_time_sec()
-{
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec;
-}
 
 static const uint8_t char_data[] = {
     0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00,
@@ -56,6 +48,8 @@ void lcd_initialize() {
     hd44780_upload_character(&lcd, 1, char_data + 8);
 }
 
+/// @brief This function clears the selected line.
+/// @param line Line to clear (0-3).
 void lcd_clear(uint8_t line) {
     // Position the cursor to the start of the line
     hd44780_gotoxy(&lcd, 0, line);
@@ -69,7 +63,7 @@ void lcd_clear(uint8_t line) {
     hd44780_gotoxy(&lcd, 0, line);
 }
 
-
+/// @brief This function clears the whole lcd-screen.
 void lcd_fullclear() {
     for (int line = 0; line < HD44780_ROWS; line++) {
         lcd_clear(line);
@@ -77,12 +71,21 @@ void lcd_fullclear() {
     hd44780_gotoxy(&lcd, 0, 0);
 }
 
+/// @brief This function writes text (at the selected location), and clears the line if preffered.
+/// @param text Text to write.
+/// @param x Starting Char in X-axis (0-19).
+/// @param y Starting line (in Y-Axis) (0-3).
+/// @param clear Bool to clear line or not.
 void lcd_write(const char *text, uint8_t x, uint8_t y, bool clear) {
     if (clear) {lcd_clear(y);}
     hd44780_gotoxy(&lcd, x, y);
     hd44780_puts(&lcd, text);
 }
 
+/// @brief This function writes the text in the center of the selected line.
+/// @param text Text to write.
+/// @param y Line to write text at (0-3).
+/// @param clear Bool to clear the line or not.
 void lcd_centerwrite(const char *text, uint8_t y, bool clear) {
 
     int center = HD44780_COLS / 2;
@@ -94,6 +97,7 @@ void lcd_centerwrite(const char *text, uint8_t y, bool clear) {
 
 }
 
+/// @brief Run this function to initialise the lcd screen.
 void lcd_init() {
     ESP_ERROR_CHECK(i2cdev_init());
     lcd_initialize();
